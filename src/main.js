9e4f1775d3610f7b901c5f01b81b763925e2c9d2
Raymond <raymond_diao@163.com>
2021-01-17 21:50:29 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import './assets/css/global.css'
+
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+Vue.prototype.$http = axios
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// message 需要进行全局挂载
+Vue.prototype.$message = Message
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
